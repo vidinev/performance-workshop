9dac111014b9761351be1f76ba6baf36225c635a
Anton <anton.vidinev@da-14.com>
2019-12-26 02:58:24 +0200

    feature(start-page) add scrolling performance tracking

--- src/app/scrolling/scrolling.component.ts
+++ src/app/scrolling/scrolling.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { UtilsService } from '../shared/utils.service';
 
 @Component({
   selector: 'ws-scrolling',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scrolling.component.scss']
 })
 export class ScrollingComponent implements OnInit {
+  items: number[] = [];
+  private calc = false;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+    this.items = (new Array(100)).fill(0);
+
+    this.document.addEventListener('scroll', () => {
+      if (this.calc) {
+        UtilsService.calculatePI(10000000);
+      }
+      this.items = this.items.map(() => Math.round(Math.random() * 500))
+    });
+  }
+
+  calcOn() {
+    this.calc = !this.calc;
+  }
+
+  trackByIndex(index) {
+    return index;
   }
 
 }
